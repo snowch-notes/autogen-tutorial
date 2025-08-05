20955f4e84c2ad5edffa9a9c66417b279ed235a9
snowch <chsnow123@gmail.com>
2025-08-05 22:06:59 +0100

    initial import

---
+++ start_jupyter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Check if .env file exists
+if [ ! -f ".env" ]; then
+  echo "Error: Create .env file with OPENAI_API_KEY=your_actual_api_key_here"
+  exit 1
+fi
+
+# how to provide .env file as param vv
+docker run -it --rm -p 8888:8888 \
+  -v "${PWD}/work":/home/jovyan/ \
+  --env-file .env \
+  quay.io/jupyter/datascience-notebook:2025-03-14
